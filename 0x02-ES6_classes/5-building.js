171b80ab3380a0db283fd9e2f77890b0dd5e41c7
hachimB <boubacarhachim@gmail.com>
2024-04-12 11:40:03 +0100

    build

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -7,7 +7,7 @@ export default class Building {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
+  evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
