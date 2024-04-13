f33cb223efe43524f5dc3fba59317a419311621a
hachimB <boubacarhachim@gmail.com>
2024-04-13 04:11:17 +0100

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+export default function handleResponseFromAPI(promise) {
+  const newPromise = new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(new Error(''));
+    }
+  });
+  newPromise.then(() => console.log('Got a response from the API'));
+}
