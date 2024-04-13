80a6a40208d575c3e389b4c2d9fa93767a78e0ea
hachimB <boubacarhachim@gmail.com>
2024-04-13 19:28:23 +0100

    Promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const [photo, user] = values;
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
