7aa814646930e2a6830dfbc29595308c3da4e1ef
hachimB <boubacarhachim@gmail.com>
2024-04-14 06:46:55 +0100

    allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -16,9 +16,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    })))
-    .then((results) => {
-      console.log(results);
-      return results;
-    });
+    })));
 }
