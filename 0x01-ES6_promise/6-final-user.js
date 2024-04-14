7029a4ab0ec47e35e041661fb1c398260e56c085
hachimB <boubacarhachim@gmail.com>
2024-04-14 02:29:15 +0100

    allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.allSettled([
+//     signUpUser(firstName, lastName),
+//     uploadPhoto(fileName),
+//   ]);
+// }
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'rejected') {
+      return { status: 'rejected', value: result.reason };
+    }
+    return result;
+  }));
 }
