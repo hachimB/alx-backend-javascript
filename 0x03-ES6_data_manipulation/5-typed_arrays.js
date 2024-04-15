af9288ea014f9cf5cabc3999b5a840ef55f0e720
hachimB <boubacarhachim@gmail.com>
2024-04-15 16:13:15 +0100

    ArrayBuffer and Int8Array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8Views = new Int8Array(buffer);
   if (position > length) {
     throw new Error('Position outside range');
   }
+  const buffer = new ArrayBuffer(length);
+  const int8Views = new Int8Array(buffer);
   int8Views[position] = value;
   return buffer;
 }
