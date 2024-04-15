076aae69f52d0cfd67dcb5904f4d647ef49dd295
hachimB <boubacarhachim@gmail.com>
2024-04-15 18:30:46 +0100

    Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const theMap = new Map();
+  theMap.set('Apples', 10);
+  theMap.set('Tomatoes', 10);
+  theMap.set('Pasta', 1);
+  theMap.set('Rice', 1);
+  theMap.set('Banana', 5);
+  return theMap;
+}
