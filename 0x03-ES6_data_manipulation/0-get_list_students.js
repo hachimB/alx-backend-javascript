93ff3cc7d5c99f8699bc2ebf0bdacf4f9676c6fa
hachimB <boubacarhachim@gmail.com>
2024-04-15 09:55:52 +0100

    arrays

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const first = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const second = { id: 2, firstName: 'James', location: 'Columbia' };
+  const third = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [first, second, third];
+}
