2a6cc07dc1a6edfe1864f91213927cf6db8f9dc4
hachimB <boubacarhachim@gmail.com>
2024-04-15 19:44:34 +0100

    WeakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,30 +1,12 @@
-// export const weakMap = new WeakMap();
-// export function queryAPI(endpoint) {
-//   let count = weakMap.get(endpoint);
-//   if (count === undefined) {
-//     count = 0;
-//   }
-//   if (count >= 5) {
-//     throw new Error('Endpoint load is high');
-//   }
-//   count += 1;
-//   weakMap.set(endpoint, count);
-// }
 export const weakMap = new WeakMap();
-
 export function queryAPI(endpoint) {
-  if (typeof endpoint !== 'object' || !endpoint.protocol || !endpoint.name) {
-    throw new Error('Invalid endpoint');
+  let count = weakMap.get(endpoint);
+  if (count === undefined) {
+    count = 0;
   }
-
-  let count = 0;
-  if (weakMap.has(endpoint)) {
-    count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
+  if (count >= 4) {
+    throw new Error('Endpoint load is high');
   }
-
-  weakMap.set(endpoint, ++count);
+  count += 1;
+  weakMap.set(endpoint, count);
 }
-
