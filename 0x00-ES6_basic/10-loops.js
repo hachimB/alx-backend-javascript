bd0ac12424f398c93d3b0fc0e9141fc6f409f533
hachimB <boubacarhachim@gmail.com>
2024-04-16 14:18:49 +0100

    for...of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(arr, appendString) {
   const newArray = [];
   for (const element of arr) {
-    newArray.push(`${element}-${appendString}`.slice(0, -1));
+    newArray.push(`${element}-${appendString}`);
   }
   return newArray;
 }
