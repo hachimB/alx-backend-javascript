91a0a2a79dfa91e3236c8ba190c5bf1687ff4770
hachimB <boubacarhachim@gmail.com>
2024-04-16 03:54:03 +0100

    Spread

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const array3 = string.split('');
-  const concat = [...array1, ...array2, ...array3];
-  return concat;
+    return [...array1, ...array2, ...string.split('')];
 }
