839e03be6602c2e4f5833bb89e2b91580981f702
hachimB <boubacarhachim@gmail.com>
2024-04-16 03:01:49 +0100

    CLeanSet

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,13 @@
 export default function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) {
+    return '';
+  }
 
+  let result = '';
+  for (let element of set) {
+    if (element.startsWith(startString)) {
+      result += `${element.replace(startString, '')}-`;
+    } 
+  }
+  return result.slice(0, -1);
 }
