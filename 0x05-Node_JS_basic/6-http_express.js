7d6b83a15f6a032e41f223340cdb8cb2ac72f037
hachimB <boubacarhachim@gmail.com>
2024-06-04 23:55:09 +0100

    A small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const port = 1245;
+const hostname = '127.0.0.1';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
