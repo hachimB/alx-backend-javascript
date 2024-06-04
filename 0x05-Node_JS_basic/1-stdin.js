c0ad840eb737875e96447977e5975f66b1db07e4
hachimB <boubacarhachim@gmail.com>
2024-06-04 10:20:46 +0100

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,20 @@
+// const readline = require('readline');
+
+// const read = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// read.on('line', (input) => {
+//   process.stdout.write(`Your name is: ${input}\n`);
+//   read.close();
+// });
+
+// read.on('close', () => {
+//   process.stdout.write('This important software is now closing\n');
+// });
 const readline = require('readline');
 
 const read = readline.createInterface({
@@ -13,5 +30,11 @@ read.on('line', (input) => {
 });
 
 read.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
+  if (process.stdin.isTTY) {
+    // The script was run interactively
+    process.stdout.write('This important software is now closing\n');
+  } else {
+    // The input came from a pipe or a file
+    process.stdout.write('This important software is now closing\n');
+  }
 });
