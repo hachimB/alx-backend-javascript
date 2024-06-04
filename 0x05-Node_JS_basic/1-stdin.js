8c5e6d93b5cb88022e607c4ede9f270f3c5efdfc
hachimB <boubacarhachim@gmail.com>
2024-06-04 10:59:57 +0100

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,31 +1,12 @@
-const readline = require('readline');
-
 process.stdin.setEncoding('utf8');
-
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-read.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\n`);
-  read.close();
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
+  process.stdin.end();
 });
 
-read.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-// process.stdin.setEncoding('utf8');
-
-// process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-// process.stdin.on('data', (input) => {
-//   process.stdout.write(`Your name is: ${input}`);
-//   process.stdin.end();
-// });
-
-// process.stdin.on('end', () => {
-//   process.stdout.write('This important software is now closing\n');
-// });
