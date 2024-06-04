80211e870ae6282bbfd3cfe27baa02ba6a3645ae
hachimB <boubacarhachim@gmail.com>
2024-06-04 08:50:15 +0100

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -17,15 +17,15 @@
 // });
 
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
   });
   
-  readline.question('Welcome to Holberton School, what is your name? \n', (name) => {
-    console.log(`Your name is: ${name}`);
-    readline.close();
+readline.question('Welcome to Holberton School, what is your name? \n', (name) => {
+  console.log(`Your name is: ${name}`);
+  readline.close();
   });
   
-  process.on('exit', () => {
-    console.log('This important software is now closing\n');
+process.on('exit', () => {
+  console.log('This important software is now closing');
   });
