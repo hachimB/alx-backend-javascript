fe78211be4ab097156fdaf5867efdd309c39be3d
hachimB <boubacarhachim@gmail.com>
2024-06-04 09:15:11 +0100

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,17 @@
-// const readline = require('readline');
+const readline = require('readline');
 
-// const read = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// console.log('Welcome to Holberton School, what is your name?');
-
-// read.on('line', (input) => {
-//   console.log(`Your name is: ${input}`);
-//   read.close();
-// });
-
-// read.on('close', () => {
-//   console.log('This important software is now closing');
-// });
-
-const readlineSync = require('readline-sync');
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 console.log('Welcome to Holberton School, what is your name?');
-const name = readlineSync.question();
-console.log(`Your name is: ${name}`);
-console.log('This important software is now closing');
+
+read.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  read.close();
+});
+
+read.on('close', () => {
+  console.log('This important software is now closing');
+});
