89805009637da2e05110062efe9ad0c50e8a6e3e
hachimB <boubacarhachim@gmail.com>
2024-06-07 09:57:17 +0100

    express test

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -6,8 +6,6 @@ const port = 7865;
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
-if  (!module.parent) {
-  app.listen(port, () => console.log(`API available on localhost port ${port}`));
-}
+app.listen(port, () => console.log(`API available on localhost port ${port}`));
 
 module.exports = app;
